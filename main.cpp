8e2cfd8b22192c304c83492173a444a5ae84d007
cs1170353 <36559702+cs1170353@users.noreply.github.com>
2019-01-11 17:17:23 +0530

    main

---
+++ main.cpp
@@ -0,0 +1,49 @@
+int main(int argc, char** argv) 
+{
+	if(argc==6)
+	{
+		if(argv[1]=="convolution_withpadding_matrixmult")
+		{
+			//call function
+		}
+		else
+		{
+			//call help()
+		}
+	}
+	else if(argc==3)
+	{
+		if(argv[1]=="activation_relu")
+		{
+			//call relu
+		}
+		else if(argv[1]=="activation_tanh")
+		{
+			//call tanh
+		}
+		else if(argv[1]=="subsampling_maxpooling")
+		{
+			//call max_pooling
+		}
+		else if(argv[1]=="subsampling_avgpooling")
+		{
+			//call avg_pooling
+		}
+		else if(argv[1]=="probability_sigmoid")
+		{
+			//call sigmoid
+		}
+		else if(argv[1]=="probability_softmax")
+		{
+			//call softmax
+		}
+		else
+		{
+			//call help()
+		}
+	}
+	else
+	{
+		//call help()
+	}
+}

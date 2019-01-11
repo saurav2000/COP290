b677564dd2039e04515092a99202248ef58a8523
saurav2000 <31991778+saurav2000@users.noreply.github.com>
2019-01-11 17:11:05 +0530

    Add files via upload

---
+++ library.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <array>
+#include <map>
+#include <vector>
+#include <list>
+#include <queue>
+#include <deque>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <ctime>
+#include <algorithm>
+#include <iterator>
+#include <cmath>
+#include <random>
+#include <bitset>
+#include <regex>
+#include <numeric>
+
+using namespace std;

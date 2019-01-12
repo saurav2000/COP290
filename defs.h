c77799d5e4d55256033e7068b1c31c1585c13741
Saurav <musunurusaurav@gmail.com>
2019-01-12 22:21:27 +0530

    Header guards

--- defs.h
+++ defs.h
@@ -1,8 +1,31 @@
+#ifndef vi
 #define vi vector<int>
+#endif
+
+#ifndef vf
 #define vf vector<float>
+#endif
+
+#ifndef vd
 #define vd vector<double>
+#endif
+
+#ifndef pii
 #define pii pair<int,int>
+#endif
+
+#ifndef pb
 #define pb push_back
+#endif
+
+#ifndef mp
 #define mp make_pair
+#endif
+
+#ifndef x
 #define x first
+#endif
+
+#ifndef y
 #define y second
+#endif

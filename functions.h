850c870da8b231ad4c0e435721bccfefff9b41c6
Saurav <musunurusaurav@gmail.com>
2019-01-13 21:59:10 +0530

    improved reference

--- functions.h
+++ functions.h
@@ -6,5 +6,5 @@ void relu(std::vector<std::vector<float>> &vec);
 void tanh(std::vector<std::vector<float>> &vec);
 void sigmoid(std::vector<float> &v);
 void softmax(std::vector<float> &v);
-std::vector<std::vector<float>> maxPool(std::vector<std::vector<float>> &v);
-std::vector<std::vector<float>> averagePool(std::vector<std::vector<float>> &v);
+std::vector<std::vector<float>>& maxPool(std::vector<std::vector<float>> &v);
+std::vector<std::vector<float>>& averagePool(std::vector<std::vector<float>> &v);

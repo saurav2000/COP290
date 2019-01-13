5116f11b3a816eb99f1df6d536d6dc2aaac6aae6
Musunuru Saurav <cs1170352@ccl1pc25.cc.iitd.ernet.in>
2019-01-14 01:56:04 +0530

    updated definitions

--- functions.h
+++ functions.h
@@ -2,9 +2,13 @@
 #include <vector>
 #endif
 
-void relu(std::vector<std::vector<float>> &vec);
-void tanh(std::vector<std::vector<float>> &vec);
-void sigmoid(std::vector<float> &v);
-void softmax(std::vector<float> &v);
-std::vector<std::vector<float>>& maxPool(std::vector<std::vector<float>> &v);
-std::vector<std::vector<float>>& averagePool(std::vector<std::vector<float>> &v);
+void relu(std::vector<std::vector<float>>& vec);
+void tanh(std::vector<std::vector<float>>& vec);
+void sigmoid(std::vector<float>& v);
+void softmax(std::vector<float>& v);
+void maxPool(std::vector<std::vector<float>>& v, std::vector<std::vector<float>>& m_pool);
+void averagePool(std::vector<std::vector<float>>& v, std::vector<std::vector<float>>& av_pool);
+void convolution_npad(std::vector<std::vector<float>>& img, std::vector<std::vector<float>>& kern, std::vector<std::vector<float>>& res);
+void convolution_pad(std::vector<std::vector<float>>& img, std::vector<std::vector<float>>& kern, int pad, std::vector<std::vector<float>>& res);
+void conv_matrmult_npad(std::vector<std::vector<float>>& img, std::vector<std::vector<float>>& kern, std::vector<std::vector<float>>& res);
+void conv_matrmult_pad(std::vector<std::vector<float>>& img, std::vector<std::vector<float>>& kern, int pad, std::vector<std::vector<float>>& res);
